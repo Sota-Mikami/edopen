ba0d81c6763a7632985bcbe33c95d2439f346580
takuya <uv.nkmt.yii@gmail.com>
2018-09-15 02:50:36 +0900

    購入の最低限の機能追加

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,31 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Stripe\Stripe;
 use Stripe\Customer;
 use Stripe\Charge;
+use App\User;
+use App\Content;
+use App\ContentImg;
+
+
 
 class CheckoutController extends Controller
 {
     public function charge(Request $request){
-        dd($request->all());
+        // dd($request->all());
         try {
             Stripe::setApiKey('sk_test_wuabzslGwSq91ILZojTz45HK');
+            $token = $request->stripeToken;
+            //ログインユーザー情報を取得
+            $user_email = Auth::user()->email;
 
-            $customer = Customer::create([
-                'email'=>$request->stripeEmail,
-                'source'=>$request->stripeToken
-            ]);
+
+            // $customer = Customer::create([
+            //     'email'=>$user_email,
+            //     'source'=>$token
+            // ]);
+            // dd($customer);
+            // tok_1DALHSDXeVxkPnaSrv5P7bDF
 
             $charge = Charge::create([
-                'customer'=>$customer->id,
                 "currency" => "jpy",
-                'amount' =>1999,
-                'currency'=>'usd',
+                'amount' =>$request->price,
+                'description'=> 'サンプル決済',
+                'source'=>$token,
+                // 'customer'=>$customer->id,
+
             ]);
 
-            return 'Charge successful, you get the course!';
+
+            $content_id = Content::find($request->id)->id;
+
+            $file_path = storage_path()."/app/public/teaching_materials/" . $content_id.'/'. $request->file_name;
+
+            return response()->download($file_path);
+            return redirect('/content/show?id='.$request->id);
+
+
+            // return 'Charge successful, you get the course!';
         } catch (\Exception $ex) {
+            dd($ex);
             return $ex->getMessage();
         }
 
